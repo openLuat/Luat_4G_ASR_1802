9b24c6d5cc840a1b7aa0e5d39e6879f54523d573
zhangtao <zhangtao@openluat.com>
2018-11-15 20:14:15 +0800

    更新uartV3的demo,规避现在的坑

--- script_LuaTask/demo/uartv3/demo.lua
+++ script_LuaTask/demo/uartv3/demo.lua
@@ -6,53 +6,49 @@
 -- @release 2018.10.20
 require "utils"
 require "pm"
+require "pins"
 module(..., package.seeall)
 
 
 -------------------------------------------- 配置串口 --------------------------------------------
 -- 串口ID,串口读缓冲区
-local UART_ID, sendQueue = 1, {}
+local UART_ID, sendQueue, writeBuff, writeBusy = 1, {}, {{}, {}}, false
 -- 串口超时，串口准备好后发布的消息
 local uartimeout, recvReady = 25, "UART_RECV_ID"
 --保持系统处于唤醒状态，不会休眠
 pm.wake("mcuart")
 uart.setup(UART_ID, 115200, 8, uart.PAR_NONE, uart.STOP_1, nil, 1)
-uart.on(1, "receive", function(uid)
-    table.insert(sendQueue, uart.read(uid, 1460))
+uart.on(UART_ID, "receive", function(uid)
+    table.insert(sendQueue, uart.read(uid, 8192))
     sys.timerStart(sys.publish, uartimeout, recvReady)
 end)
-local count = 0
-uart.on(1, "sent", function()
-    sys.publish("done")
-    count = count + 1
-    log.info("send done", count)
+uart.on(UART_ID, "sent", function()
+    if #writeBuff[1] == 0 then
+        writeBusy = false
+        sys.publish("done")
+        log.info("uart 1 send done")
+    else
+        writeBusy = true
+        uart.write(UART_ID, table.remove(writeBuff[1], 1))
+        log.info("uart 1 send ing...")
+    end
 end)
+function write(uid, str)
+    for i = 1, #str, 8192 do
+        table.insert(writeBuff[1], str:sub(i, i + 8192 - 1))
+    end
+    log.info("串口缓冲队列的长度:", writeBusy, #table.concat(writeBuff[1]))
+    if not writeBusy then
+        writeBusy = true
+        uart.write(uid, table.remove(writeBuff[1], 1))
+    end
+end
+
 -- 向串口发送收到的字符串
 sys.subscribe(recvReady, function()
     local str = table.concat(sendQueue)
-    log.info("uart read length:", #str, str)
-    -- 串口写缓冲区最大1460
-    for i = 1, #str, 1460 do
-        uart.write(UART_ID, str:sub(i, i + 1460 - 1))
-    end
     -- 串口的数据读完后清空缓冲区
     sendQueue = {}
+    log.info("uart read length:", #str)
+    write(UART_ID, str)
 end)
-
-sys.taskInit(function()
-    local str = string.rep("s", 120000)
-    while true do
-        for i = 1, #str, 1000 do
-            uart.write(UART_ID, str:sub(i, i + 1000 - 1))
-            log.info("test 100K uart data", #str:sub(i, i + 1000 - 1))
-            sys.waitUntil("done")
-        end
-        sys.wait(10000)
-    end
-end)
-
--- for i = 0, 120000, 1000 do
---     uart.write(uartID, spiFlash.read_m(i, 1000))
--- end
-
-sys.timerLoopStart(log.info, 1000, "测试打印的数据方法：", "test")
